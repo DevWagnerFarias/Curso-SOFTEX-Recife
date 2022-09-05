9be191fc3b3c576fadef309b892ae390278d29eb
DevWagnerFarias <wagnerdefarias12@gmail.com>
2022-09-05 20:11:22 -0300

    Back-end Node

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require ('express')
+
+const app = express()
+
+const port = 3000
+
+app.get ('/', (req, res) =>{
+    res.send("Hello World")
+})
+
+app.listen(port, () => {
+    console.log('O servidor está sendo exacutado em http:localhost:' , port)
+})
